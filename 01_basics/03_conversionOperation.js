38d6948cd4dfec8f1e94f64caa4604c8a5b67c9d
suraj-gu <surajaggarwal62068@gmail.com>
2025-12-22 11:42:42 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" = 33
+// "33abc" = NaN(not a number)
+// true = 1; false = 0;
+
+let isLoggedIn = "suraj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+   1 => true; 0 => false
+   "" => flase; "Suraj" => true
+   
+*/
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+
+console.log(typeof stringSomeNumber);
+console.log(stringSomeNumber);
+
+
