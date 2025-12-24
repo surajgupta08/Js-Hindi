7e597e4dd4a43ffe2b1f0a19fc9b9101b51767c1
suraj-gu <surajaggarwal62068@gmail.com>
2025-12-24 13:59:48 +0000

    initial commit

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,40 @@
+let name = "Suraj"
+let repoCount = 50
+
+// console.log(`My name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String("Suraj Rc com")
+
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(3));
+
+// console.log(gameName.indexOf('R'));
+
+const newString = gameName.substring(0,5)
+// console.log(newString);
+
+const anotherString = gameName.slice(-6,4)
+// console.log(anotherString);
+
+const stringOne = "   Suraj   "
+// console.log(stringOne);
+// console.log(stringOne.trim());
+
+const url = "https://suraj.com/suraj%20kumar"
+
+// console.log(url.replace("%20","-"));
+
+// console.log(url.includes("suraj"));
+
+console.log(gameName.split(" ",2));
+
+
+
+
+
+
