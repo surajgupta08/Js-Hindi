6c370afc37e0437ce68c2b1291b455fe8eb3b2e6
suraj-gu <surajaggarwal62068@gmail.com>
2025-12-24 14:46:57 +0000

    Nums and Maths

---
+++ 01_basics/07_numsAndMaths.js
@@ -0,0 +1,37 @@
+const score = 100;
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+// console.log(balance.toString().length);
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toFixed(2));
+
+// console.log(otherNumber.toPrecision(3));
+
+const bigNumber = 1000000;
+// console.log(bigNumber.toLocaleString('en-in'));
+
+
+// +++++++++++ Maths ++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2)); 
+// console.log(Math.floor(1.9));
+
+// console.log(Math.floor(Math.random()*6)+1);// This will give random number bt 1-6
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max-min+1) + min));
+
+
+
+
+
+
