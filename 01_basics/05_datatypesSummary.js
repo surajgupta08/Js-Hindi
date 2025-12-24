121f85b1bb38b42d1a9a4e55a1968f0bb51a6304
suraj-gu <surajaggarwal62068@gmail.com>
2025-12-24 05:02:59 +0000

    Stack and Heap discussion

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -13,31 +13,31 @@
 
 // 1. Number
 let num = 42;
-console.log(typeof num); // "number"
+// console.log(typeof num); // "number"
 
 // 2. String
 let str = "Hello, World!";
-console.log(typeof str); // "string"
+// console.log(typeof str); // "string"
 
 // 3. Boolean
 let bool = true;
-console.log(typeof bool); // "boolean"
+// console.log(typeof bool); // "boolean"
 
 // 4. Undefined
 let undef;
-console.log(typeof undef); // "undefined"   
+// console.log(typeof undef); // "undefined"   
 
 // 5. Null
 let nul = null;
-console.log(typeof nul); // "object" (this is a known quirk in JavaScript)
+// console.log(typeof nul); // "object" (this is a known quirk in JavaScript)
 
 // 6. Symbol
 let sym = Symbol("unique");
-console.log(typeof sym); // "symbol"
+// console.log(typeof sym); // "symbol"
 
 // 7. BigInt
 let bigInt = 9007199254740991n;
-console.log(typeof bigInt); // "bigint"
+// console.log(typeof bigInt); // "bigint"
 
 // Note: Primitive datatypes are immutable and are compared by value.
 
@@ -49,15 +49,41 @@ console.log(typeof bigInt); // "bigint"
 
 // 1. Object
 let obj = { name: "Alice", age: 30 };
-console.log(typeof obj); // "object"
+// console.log(typeof obj); // "object"
 
 // 2. Array
 let arr = [1, 2, 3, 4, 5];
-console.log(typeof arr); // "object" (arrays are a type of object)
+// console.log(typeof arr); // "object" (arrays are a type of object)
 
 // 3. Function
 let func = function() { return "Hello"; };
-console.log(typeof func); // "function"
+// console.log(typeof func); // "function"
 
 // Note: Non-primitive datatypes are mutable and are compared by reference. 
 // Changes made to one reference will affect all references to that object.
+
+
+//  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) and Heap(Non-Primitive)
+
+let myName = "Suraj"
+let mySecondName = "myName"
+
+// mySecondName = "Badal"
+// console.log(myName);
+// console.log(mySecondName);
+
+let obj1 = {
+    name: "Suraj",
+    lastName: "Gupta"
+    }
+
+let obj2 = obj1;
+
+obj2.name = "Badal"
+console.log(obj1.name);
+console.log(obj2.name);
+
+
+
