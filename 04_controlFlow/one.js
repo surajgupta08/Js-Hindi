67d7a60fe4216020faa97e260d340835dc0e9b10
suraj-gu <surajaggarwal62068@gmail.com>
2026-01-21 18:38:09 +0000

    basic if else

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,52 @@
+let isLoggedIn = true;
+
+// if(2 === '2'){
+//     console.log("excuted");
+// }else{
+//     console.log(`not Excuted`);
+
+// }
+
+//     operators
+// < , > , <=, >= , != , == , ===, !==
+
+let score = 200;
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+
+// }
+// console.log(`User power: ${power}`);
+
+const userLoggedIn = true;
+const debitCard = true;
+const userLoggedInWithGoogle = false;
+const userLoggedInWithGmail = true;
+
+// if (userLoggedIn && debitCard) {
+//   console.log(`Allow to buy a course.`);
+// }
+
+// if(userLoggedInWithGoogle || userLoggedInWithGmail){
+//     console.log(`Logged in!`);
+    
+// }
+
+// Null coalescing Operator  null , undefine.
+
+let val1
+// val1 = 10 ?? 15
+// val1 = null ?? 15
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+
+
+
+
+
+
+
+
+console.log(val1);
