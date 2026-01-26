fe21a57e6532af503ca715f19165a84045cfcfc0
suraj-gu <surajaggarwal62068@gmail.com>
2026-01-26 14:31:55 +0000

    while & doWhile loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+
+
+let num = 0
+while (num <= 10) {
+    // console.log(num);
+    num = num + 2
+}
+
+let myArray = ["Flash","Superman","Batman"]
+let index = 0
+
+while (index < myArray.length) {
+    // console.log(myArray[index]);
+    index++
+    
+}
+
+let score = 11
+
+do {
+    console.log(`Value is: ${score}`);
+    score++
+} while (score <= 10);
