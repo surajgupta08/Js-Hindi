0fa1a8e2ca336b18d670b3be46d6926b99e5ec39
suraj-gu <surajaggarwal62068@gmail.com>
2026-01-31 08:45:29 +0000

    forin loop for object

---
+++ 05_iterations/four.js
@@ -0,0 +1,21 @@
+const myObject = {
+    "js":"JavaScript",
+    "cpp":"C plus plus",
+    "py":"Python"
+}
+
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(`${key} short form for ${myObject[key]}`);
+    
+    
+}
+
+
+const coding = ["js","cpp","java","ruby"]
+
+for (const key in coding) {
+    console.log(coding[key]);
+    
+    
+}
